e20b649be9abf333702ea04c63f129cbdd8bff25
Owain Lewis <owain@owainlewis.com>
2016-01-15 11:13:15 +0000

    Update Compiler.hs

--- src/Language/Compiler.hs
+++ src/Language/Compiler.hs
@@ -1,8 +1,4 @@
 module Language.Compiler where
 
-import           Data.Aeson
-import           Language.AST    (Entity (..))
-import qualified Language.Parser as Parser
-
 class Renderable a where
     render :: a -> String
